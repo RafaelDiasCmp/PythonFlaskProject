599fdec529871268fcce890c369dc38ed2fe2c26
Rafael Dias Campos <73750287+RafaelDiasCmp@users.noreply.github.com>
2024-06-04 21:55:10 -0400

    Alterações nos templates - Exibir - Adicionar - Deletar Produtos utilizando BD Local

--- database/resp.py
+++ database/resp.py
@@ -1,7 +1,7 @@
-RESPONSAVEL = [
-    {"id": 1, "nome": "Guilherme", "email": "raffaeldiascampos@gmail.com"},
-    {"id": 2, "nome": "Jamel", "email": "fafafafafaos@gmail.com"},
-    {"id": 3, "nome": "Cleber", "email": "qeqeqeail.com"},
-    {"id": 4, "nome": "Jesus", "email": "asdasdos@gmail.com"},
-    {"id": 5, "nome": "Gerrard", "email": "asdasds@gmail.com"},
+PRODUTO = [
+    {"id": 1, "nome": "Bolo", "preco": "25,00", "qtde": "3"},
+    {"id": 2, "nome": "Brigadeiro", "preco": "10,00", "qtde": "3"},
+    {"id": 3, "nome": "Beijinho", "preco": "4,00", "qtde": "3"},
+    {"id": 4, "nome": "Bicho-de-pé", "preco": "7,00", "qtde": "3"},
+    {"id": 5, "nome": "Paçoca", "preco": "5,00", "qtde": "3"},
 ]
--- routes/responsavel.py
+++ routes/responsavel.py
@@ -1,19 +1,31 @@
-from flask import Blueprint, render_template
-from database.resp import RESPONSAVEL
+from flask import Blueprint, render_template, request
+from database.resp import PRODUTO
 
 responsavel_route = Blueprint('responsavel', __name__)
 
 @responsavel_route.route('/')
 def lista_responsavel():
-    return render_template('lista_responsavel.html', responsavel=RESPONSAVEL)
+    return render_template('lista_produtos.html', produto=PRODUTO)
 
 @responsavel_route.route('/', methods=['POST'])
-def inserir_responsavel(): #inserir os dados do responsavel
-    pass
+def inserir_responsavel(): #inserir os dados do produto
+    
+    data = request.json
+    
+    novo_produto = {
+        "id": len(PRODUTO) + 1,
+        "nome": data['nome'],
+        "preco": data['preco'],
+        "qtde": data['qtde'],
+    }
+
+    PRODUTO.append(novo_produto)
+
+    return render_template('item_produto_responsavel.html', produto=novo_produto)
 
 @responsavel_route.route('/new')
-def form_responsavel(): #cadastrar um responsavel
-    return render_template('criar_produto_responsavel.html')
+def form_responsavel(): #cadastrar um produto/responsavel
+    return render_template('form_produtos.html')
 
 @responsavel_route.route('/<int:responsavel_id>')
 def detalhe_responsavel(cliente_id): # exibir detalhes responsavel
@@ -28,7 +40,11 @@ def atualizar_form_responsavel(cliente_id): # atualizar detalhes responsavel
     pass
 
 @responsavel_route.route('/<int:responsavel_id>/delete', methods=['DELETE'])
-def deletar_form_responsavel(cliente_id): # deletar detalhes responsavel
-    pass
+def deletar_produto(responsavel_id): # deletar detalhes responsavel
+    
+    global PRODUTO
+    PRODUTO = [ c for c in PRODUTO if c['id'] != responsavel_id ]
+
+    return {'deleted': 'ok'}
 
 
