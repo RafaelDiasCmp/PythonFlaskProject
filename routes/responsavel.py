b851cc764b05ad06da814b9b369685ccdf7b41e7
Rafael Dias Campos <73750287+RafaelDiasCmp@users.noreply.github.com>
2024-06-10 17:28:31 -0400

    EDITAR e EXIBIR/VER funcional com BD Local no arquivo

--- routes/responsavel.py
+++ routes/responsavel.py
@@ -4,11 +4,11 @@ from database.resp import PRODUTO
 responsavel_route = Blueprint('responsavel', __name__)
 
 @responsavel_route.route('/')
-def lista_responsavel():
+def lista_produtos():
     return render_template('lista_produtos.html', produto=PRODUTO)
 
 @responsavel_route.route('/', methods=['POST'])
-def inserir_responsavel(): #inserir os dados do produto
+def inserir_produtos(): #inserir os dados do produto
     
     data = request.json
     
@@ -24,26 +24,50 @@ def inserir_responsavel(): #inserir os dados do produto
     return render_template('item_produto_responsavel.html', produto=novo_produto)
 
 @responsavel_route.route('/new')
-def form_responsavel(): #cadastrar um produto/responsavel
+def form_produtos(): #cadastrar um produto
     return render_template('form_produtos.html')
 
-@responsavel_route.route('/<int:responsavel_id>')
-def detalhe_responsavel(cliente_id): # exibir detalhes responsavel
-    return render_template('detalhe_responsavel.html')
+@responsavel_route.route('/<int:produto_id>')
+def detalhe_produtos(produto_id): # exibir detalhes responsavel
+    
+    produto = list(filter(lambda c: c['id'] == produto_id, PRODUTO))[0]
+    return render_template('detalhe_produto.html', produto=produto)
 
-@responsavel_route.route('/<int:responsavel_id>/edit')
-def editar_form_responsavel(cliente_id): # editar detalhes responsavel
-    return render_template('edit_form_responsavel.html')
+@responsavel_route.route('/<int:produto_id>/edit')
+def editar_form_produtos(produto_id): # editar detalhes do produto
+    
+    produto = None
+    for c in PRODUTO:
+        if c['id'] == produto_id:
+            produto = c
 
-@responsavel_route.route('/<int:responsavel_id>/update', methods=['PUT'])
-def atualizar_form_responsavel(cliente_id): # atualizar detalhes responsavel
-    pass
+    return render_template('form_produtos.html', produto=produto)
 
-@responsavel_route.route('/<int:responsavel_id>/delete', methods=['DELETE'])
-def deletar_produto(responsavel_id): # deletar detalhes responsavel
+@responsavel_route.route('/<int:produto_id>/update', methods=['PUT'])
+def atualizar_form_produtos(produto_id): # atualizar detalhes do produto
+    
+    produto_editado = None
+    
+    #obtendo dados do form
+    data = request.json
+
+    #obter usuário pelo id
+    for c in PRODUTO:
+        if c['id'] == produto_id:
+            c['nome'] = data['nome']
+            c['preco'] = data['preco']
+            c['qtde'] = data['qtde']
+
+            produto_editado = c
+    # editar usuário
+    return render_template('item_produto_responsavel.html', produto=produto_editado)
+
+
+@responsavel_route.route('/<int:produto_id>/delete', methods=['DELETE'])
+def deletar_produto(produto_id): # deletar detalhes do produto
     
     global PRODUTO
-    PRODUTO = [ c for c in PRODUTO if c['id'] != responsavel_id ]
+    PRODUTO = [ c for c in PRODUTO if c['id'] != produto_id ]
 
     return {'deleted': 'ok'}
 
